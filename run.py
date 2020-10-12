89b3faf65815f8503fae5d334633adb35f3c5c2f
artchiee <53340008+artchiee@users.noreply.github.com>
2020-10-12 17:54:46 +0100

    proc error may br solved

--- run.py
+++ run.py
@@ -1,8 +1,28 @@
-from validation import app
+
+from flask import Flask
 import os
+from validation.contact import Contact
+
+# app initialazation
+app = Flask(__name__)
+
+# setting with "ENV" to run
+if app.config["ENV"] == "production":
+    app.config.from_object("config.ProductionConfig")
+else:
+    app.config.from_object("config.DevelopmentConfig")
+
+# check wich ENV running whene server starts
+print(f'ENV is set to: {app.config["ENV"]}')
+
+# register static directory
+app._static_folder = '../static'
 
 # Setting secrte_key (Developpement)
 app.secret_key = os.environ.get('Key')
+
+# Register Blueprints
+app.register_blueprint(Contact)
 
 # Must be set/changed on Production Mode
 if __name__ == "__main__":
