ee4d62561c543395bc88dd5df98cc508880fabdc
Nouamanezh909 <nouamanezh99@gmail.com>
2020-10-12 18:11:40 +0100

    proc prob

---
+++ validation/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from .contact import Contact
+
+# app initialazation
+app = Flask(__name__)
+
+# setting with "ENV" to run
+if app.config["ENV"] == "production":
+    app.config.from_object("config.ProductionConfig")
+else:
+    app.config.from_object("config.DevelopmentConfig")
+
+# check wich ENV running whene server starts
+print(f'ENV is set to: {app.config["ENV"]}')
+
+# register static directory
+app._static_folder = '../static'
+
+# Register Blueprints
+app.register_blueprint(Contact)
