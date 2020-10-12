839ba53c58ee95487bf2c83da15f8abc2bbf6745
artchiee <53340008+artchiee@users.noreply.github.com>
2020-10-12 18:11:40 +0100

    proc prob

--- run.py
+++ run.py
@@ -1,28 +1,11 @@
-
-from flask import Flask
+# initiat project by running python run.py  
+from validation import app
 import os
-from validation.contact import Contact
+from flask import Flask
 
-# app initialazation
-app = Flask(__name__)
-
-# setting with "ENV" to run
-if app.config["ENV"] == "production":
-    app.config.from_object("config.ProductionConfig")
-else:
-    app.config.from_object("config.DevelopmentConfig")
-
-# check wich ENV running whene server starts
-print(f'ENV is set to: {app.config["ENV"]}')
-
-# register static directory
-app._static_folder = '../static'
 
 # Setting secrte_key (Developpement)
 app.secret_key = os.environ.get('Key')
-
-# Register Blueprints
-app.register_blueprint(Contact)
 
 # Must be set/changed on Production Mode
 if __name__ == "__main__":
