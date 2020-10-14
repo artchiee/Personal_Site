73ac9dc3f3e983856f1550efad80a9585d5253af
Nouamanezh909 <nouamanezh99@gmail.com>
2020-10-14 13:42:42 +0100

    Proc error solved

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
 # initiat project by running python run.py  
 from validation import app
-import os
-from flask import Flask
 
-
-# Setting secrte_key (Developpement)
-app.secret_key = os.environ.get('Key')
 
 # Must be set/changed on Production Mode
 if __name__ == "__main__":
