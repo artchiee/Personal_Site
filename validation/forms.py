31dd368a287ba96f36e824235a7ba5390afc8ebc
Nouamanezh909 <nouamanezh99@gmail.com>
2020-10-22 15:20:46 +0100

    protocol issues

--- validation/forms.py
+++ validation/forms.py
@@ -12,7 +12,7 @@ class ContactForm(FlaskForm):
     name = StringField('Name', 
             [validators.DataRequired()])
 
-    user_mail = EmailField('Email', [validators.DataRequired()])
+    user_mail = EmailField('Email', [validators.DataRequired(message='Only gmail allowed here !!g')])
 
     subject = TextField("Subject")
     message = TextAreaField("Message",[validators.DataRequired()])
